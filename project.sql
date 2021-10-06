6ef7eb6bccb9b5116f52811e14ec47b2538cc416
alroyhery <90199997+alroyhery@users.noreply.github.com>
2021-10-06 20:32:19 +0700

    sql baru
    baru

--- project.sql
+++ project.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 05, 2021 at 06:09 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.3
+-- Generation Time: Oct 06, 2021 at 03:31 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,16 +28,18 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `category` (
-  `id_category` int(11) NOT NULL,
-  `categoryname` varchar(255) NOT NULL
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `category`
 --
 
-INSERT INTO `category` (`id_category`, `categoryname`) VALUES
-(1, 'horror');
+INSERT INTO `category` (`id`, `name`, `description`) VALUES
+(1, 'horror', NULL),
+(2, 'scifi', NULL);
 
 -- --------------------------------------------------------
 
@@ -46,20 +48,19 @@ INSERT INTO `category` (`id_category`, `categoryname`) VALUES
 --
 
 CREATE TABLE `comment` (
-  `id_comment` int(11) NOT NULL,
-  `content` text NOT NULL
+  `id` int(11) NOT NULL,
+  `content` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `comment`
 --
 
-INSERT INTO `comment` (`id_comment`, `content`) VALUES
-(1, 'asd'),
-(4, 'asdadsasdasd'),
-(5, 'komentar 1'),
-(6, 'komentar 2'),
-(7, 'asdasdasdasd');
+INSERT INTO `comment` (`id`, `content`) VALUES
+(1, 'qwerty'),
+(3, 'zxcvbn'),
+(4, '1asda3234'),
+(5, 'adly gay');
 
 -- --------------------------------------------------------
 
@@ -86,9 +87,8 @@ CREATE TABLE `thread` (
   `id` int(11) NOT NULL,
   `title` varchar(230) NOT NULL,
   `content` text NOT NULL,
-  `category` text NOT NULL,
-  `id_category` int(11) DEFAULT NULL,
-  `id_user` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -96,11 +96,8 @@ CREATE TABLE `thread` (
 -- Dumping data for table `thread`
 --
 
-INSERT INTO `thread` (`id`, `title`, `content`, `category`, `id_category`, `id_user`, `created_at`) VALUES
-(1, 'CERITAKU DI RUMAH', 'Malam itu aku mlihat aloroy sedang di kamar dan aksdjasdkjaskdjaskdjaskdjaksdjkasjdkjasdkjasdkjaskjd kemudian dia asdaskdjaskdjkasdkjaskjd lalu dia askjdkasjdkasdjasjdkjaskdaskdjaksdasdasd', 'horror', 0, 1, '2021-10-02 23:46:01'),
-(2, 'AKU ADA IDE', 'Ada beberapa jenis media sosial yang populer dan banyak digunakan semua kalangan, seperti Instagram, Facebook, Twitter, hingga Whatsapp.\r\nKamu juga dapat mem-posting foto atau video ke media sosial. Saat mengunggahnya, kamu biasanya membutuhkan kata-kata yang dijadikan sebagai caption.\r\nKata-kata tersebut berguna untuk membuat postingan yang kamu buat menjadi lebih keren dan menarik sehingga banyak orang yang tertarik dengan postinganmu.\r\nHal tersebut dibutuhkan karena pada era modern saat ini banyak orang yang membutuhkan eksistensi untuk kehidupan bersosial.\r\nNamun, tidak semua orang dapat merangkai kata-kata keren untuk dijadikan sebagai caption di berbagai media sosial yang mereka miliki.', 'EDUKATIF', NULL, NULL, '2021-10-04 20:16:48'),
-(3, 'ERLANGGA', 'AKU PUNYA TEMAN NAMANYA ERLANGGA DIA SELALU ASDKASJDKASDKASJDKASJDKASDJ KEMUDIAN ASKDASKDJASKDJASKJDKASD LALU ASKDAKSDJAKSJDKASKDJAS DAN AKSDJASKDJASKJDKASJDKDASJ SAAT ASKDNASKDAKSDNASDNASDASD', 'psychology horror', NULL, NULL, '2021-10-04 21:27:48'),
-(5, 'asdlasdasdkasdkk', 'asdasdasdasdasdaasdasdasdad askdjaskdjaskdkaskdjaskj aksj askdjaksjdaskjdkj akjd kasjd kasjdaksdjasldjasldjalsj lj alskjdaskljdlaskjdlas kasdjkjkjk askdjaksdjakj kj aksjdaksjdkja skdjka d', 'Comedy', NULL, NULL, '2021-10-04 21:39:24');
+INSERT INTO `thread` (`id`, `title`, `content`, `user_id`, `category_id`, `created_at`) VALUES
+(2, 'adly sang pendekar pedang', 'di suatu hari aku sedang jogmafo;lfkapsdad', 1, 2, '2021-10-06 20:19:56');
 
 -- --------------------------------------------------------
 
@@ -121,8 +118,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `email`, `password`, `created_at`) VALUES
-(1, 'qwerty', '123@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', '2021-10-02 23:13:55'),
-(2, 'adly', 'adly@a.com', '202cb962ac59075b964b07152d234b70', '2021-10-04 20:57:50');
+(1, 'qwerty', '123@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', '2021-10-02 23:13:55');
 
 --
 -- Indexes for dumped tables
@@ -132,13 +128,13 @@ INSERT INTO `user` (`id`, `name`, `email`, `password`, `created_at`) VALUES
 -- Indexes for table `category`
 --
 ALTER TABLE `category`
-  ADD PRIMARY KEY (`id_category`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `comment`
 --
 ALTER TABLE `comment`
-  ADD PRIMARY KEY (`id_comment`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `report`
@@ -153,7 +149,9 @@ ALTER TABLE `report`
 -- Indexes for table `thread`
 --
 ALTER TABLE `thread`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `category_id` (`category_id`);
 
 --
 -- Indexes for table `user`
@@ -169,13 +167,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `id_comment` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `report`
@@ -187,13 +185,24 @@ ALTER TABLE `report`
 -- AUTO_INCREMENT for table `thread`
 --
 ALTER TABLE `thread`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `thread`
+--
+ALTER TABLE `thread`
+  ADD CONSTRAINT `thread_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `thread_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
