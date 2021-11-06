c0fc6fbca8d263de14660a3f317386f122839968
AdlyRadwika <aradwika@gmail.com>
2021-11-06 13:38:40 +0700

    Penambahan tanggal pada thread

--- src/test/java/com/tugas/project1/EditThreadIntegrationTests.java
+++ src/test/java/com/tugas/project1/EditThreadIntegrationTests.java
@@ -29,7 +29,7 @@ public class EditThreadIntegrationTests {
     @Autowired
     ThreadService service;
 
-    @Mock //MockBean
+    @MockBean //MockBean
     ThreadRepository repository;
 
     @Mock
